9ce6a40c2e85989e64648cf3066891d5f82ed3a5
ganeshraavan <101339784+ganeshraavan@users.noreply.github.com>
2022-03-10 19:39:44 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,18 @@
+def solve(N,M,A,B):
+    dic ={}
+    for i in range(N):
+        dic[A[i]] = i
+    c = [ i for i  in A]
+    c.sort()
+    c = c[N-M:]
+    minVal = min(c)
+    ind = []
+    for i in c:
+        ind.append(dic[i])
+    minVal = min(c)
+    sum =0
+    for i in ind:
+        sum = sum + B[i]
+    return sum * minVal
+
+print(solve(2,2,[6,10],[7,7]))
